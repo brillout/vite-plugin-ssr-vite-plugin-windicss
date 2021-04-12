2127d9a07091d94bd598d1c22166ae59bc584903
Romuald Brillout <git@brillout.com>
2021-04-12 19:26:49 +0200

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import ssr from "vite-plugin-ssr/plugin";
+import WindiCSS from "vite-plugin-windicss";
 
 export default {
-  plugins: [reactRefresh(), ssr()],
+  plugins: [reactRefresh(), ssr(), WindiCSS()],
 };
